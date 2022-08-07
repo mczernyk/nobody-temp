a15d23d632e6e77d4aebe347c2792503444bd881
mczernyk <mczernyk@gmail.com>
2022-08-07 17:25:18 -0400

    updated about

--- pages/about.js
+++ pages/about.js
@@ -7,9 +7,9 @@ export default function about() {
     <div className="about-container">
       <h1>about</h1>
         <div className='about-content'>
-          <p>nobody allows you to create products from a curated group of NFT collections using your CC0 assets. you own your NFTs, now you can do something with them.</p>
+          <p>nobody allows you to create products from a curated group of NFT collections using assets in your wallet. you own your NFTs, now you can do something with them.</p>
 
-          <p>nobody also sells original gear and pieces featuring CC0 assets from nobody's vault. NFTs can be expensive, nobody gives customers a different way to support these projects. the offerings from each collection will be rotated periodically based on nobody's holdings.</p>
+          <p>nobody also sells original gear and pieces featuring assets from nobody's vault. NFTs can be expensive, nobody gives customers a different way to support these projects. the offerings from each collection will be rotated periodically based on nobody's holdings.</p>
 
           <p>nobody uses 20% of profits from sales to sweep the floor of each respective collection. nobody does so once this profit allocation reaches the floor price.</p>
 
