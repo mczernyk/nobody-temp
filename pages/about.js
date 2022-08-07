6af7f947176995bee9d4e9eeef9f90b0fc7388f0
mczernyk <mczernyk@gmail.com>
2022-08-07 02:05:48 -0400

    updated about

--- pages/about.js
+++ pages/about.js
@@ -9,13 +9,13 @@ export default function about() {
         <div className='about-content'>
           <p>nobody offers products from a curated list of NFT collections featuring your CC0 assets. you own your NFTs, now you can do something with them.</p>
 
-          <p>nobody also offers original gear as well as pieces from these collections featuring CC0 assets from nobody's vault. NFTs can be expensive, nobody allows customers to support projects without owning their assets. the choices for each collection will be rotated periodically based on holdings in nobody's vault.</p>
+          <p>nobody also offers original gear and pieces from these collections featuring CC0 assets from nobody's vault. NFTs can be expensive, nobody allows customers to support projects without owning their assets. the pieces from each collection will be rotated periodically based on holdings in nobody's vault.</p>
 
           <p>nobody uses 20% of profits from sales to sweep the floor of each respective collection. nobody does so once this profit allocation reaches the floor price.</p>
 
           <p>nobody limits the amount of products sold daily. currently 25 orders are accepted per day. the store reopens every day at 11AM EST.</p>
 
-          <p>nobody offers a 10% collection discount for the best selling collection for one day each month. this sale day is announced on twitter.</p>
+          <p>nobody offers a 10% collection discount for each month's best selling collection. this sale lasts 24 hours, the sale date is announced on <a className='twitter-about' href="https://twitter.com/nobody_clothing">twitter</a>.</p>
 
           <h2>♡ nobody loves you ♡</h2>
           <h3>
