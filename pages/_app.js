e8d818853ee4ef41582395a6d8d7aa57467796f7
mczernyk <mczernyk@gmail.com>
2022-08-07 02:34:05 -0400

    updated css

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,13 @@ import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return (
+
     <StateContext>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </StateContext>
+
 
   )
 }
