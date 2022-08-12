90217825d643773c06cac971609a00a1770c21da
mczernyk <mczernyk@gmail.com>
2022-08-12 05:34:25 -0400

    added nnn

--- components/Previews.jsx
+++ components/Previews.jsx
@@ -10,8 +10,9 @@ const Previews = () => {
 
     const picsArray = [
       '/images/milady2black.png',
-      '/images/loveberry.png',
+      '/images/nnnpurple.png',
       '/images/miaurawhite.png',
+      '/images/loveberry.png',
       '/images/cdb1purple.png',
       '/images/allstarz2berry.png',
       '/images/passenger black.png',
